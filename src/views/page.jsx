8bf112ca17a1cad5e37919b1b437e52e2d25b155
Mac User <macuser@macs-mbp.gateway.pace.com>
2017-08-28 20:41:01 -0700

    add form-example files with color change on submit

--- src/views/page.jsx
+++ src/views/page.jsx
@@ -17,7 +17,7 @@ class Page extends React.Component {
             <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet justo donec enim diam vulputate ut pharetra sit. Nunc congue nisi vitae suscipit tellus. Orci sagittis eu volutpat odio. Eget aliquet nibh praesent tristique magna. Id cursus metus aliquam eleifend mi in nulla. Nulla facilisi etiam dignissim diam quis. Diam in arcu cursus euismod quis viverra nibh cras. Sit amet nisl suscipit adipiscing. Velit laoreet id donec ultrices tincidunt arcu non sodales. Ultricies mi eget mauris pharetra et ultrices neque ornare. Facilisis mauris sit amet massa vitae tortor condimentum. Justo laoreet sit amet cursus. Quam vulputate dignissim suspendisse in est ante in. Ultrices in iaculis nunc sed augue. In pellentesque massa placerat duis ultricies lacus sed. Sodales ut eu sem integer vitae justo. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Mattis pellentesque id nibh tortor id. Egestas egestas fringilla phasellus faucibus scelerisque eleifend.</span>
           </div>
           <div className="col">
-            <ReactForm color="beige" height = "200" />
+            <ReactForm />
             <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet justo donec enim diam vulputate ut pharetra sit. Nunc congue nisi vitae suscipit tellus. Orci sagittis eu volutpat odio. Eget aliquet nibh praesent tristique magna. Id cursus metus aliquam eleifend mi in nulla. Nulla facilisi etiam dignissim diam quis. Diam in arcu cursus euismod quis viverra nibh cras. Sit amet nisl suscipit adipiscing. Velit laoreet id donec ultrices tincidunt arcu non sodales.</span>
           </div>
           <div className="col">
@@ -32,18 +32,18 @@ class Page extends React.Component {
 class ReactForm extends React.Component {
   constructor(props) {
     super(props);
-    //this refers to whole class
+
     this.state = {
       submitted: false,
-      color: this.props.color
+      color: 'darkSeaGreen'
     };
-    //console.log(this.state.submitted);
 
     this.submit = this.submit.bind(this);
 }
+
   render() {
     if (!this.state.submitted) {
-      alert ('Not Submitted');
+      alert ('Notice: Your form has not been submitted');
     return (
       <form className = {this.state.color}>
         <label htmlFor="name">Name:</label>
@@ -57,14 +57,21 @@ class ReactForm extends React.Component {
     );
   } else {
     return (
-      <h1>Thank you for submitting!</h1>
+      <div className = {this.state.color}>
+      <h2 className = 'center'>We got it!<br />Thank you for submitting!</h2>
+      </div>
     )
   }
 }
+
+// MESSAGE FROM JESSICA!
+// NOTE: submit will change TEXT and BACKGROUND COLOR of FORM AREA!
+
   submit() {
     this.setState((state) =>
       state.submitted = !state.submitted
-  )};
+  ), this.setState( {color: 'darkSalmon'});
+}
 }
 
 ReactDOM.render(<Page />, document.getElementById('container'));
